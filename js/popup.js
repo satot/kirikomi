1f60a9ceb61426dd5bd05754efcbe0ff4463a49f
satot <ph1.ph2.sa25@gmail.com>
2014-07-20 19:58:37 +0700

    first commit

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('#alpha-inner').css('cssText', 'background:#ccc !important');
+  $('.entry-content p').css('color', '#000');
+  $('.content-nav').css('color', '#000');
+  $('.entry-body a').css('color', '#186939');
+  $('.content-nav a').css('color', '#186939');
+  $('#beta-inner').remove();
+});
